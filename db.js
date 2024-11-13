4e9fc42d50649607c8c13ea4615697208aecfa84
Fabian Arredondo <fabianarredondo365@gmail.com>
2024-11-13 16:53:02 -0600

    Commit antes del push

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'malparidos20', // Cambia esto por tu contraseña de MySQL
+    password: 'password', // Cambia esto por tu contraseña de MySQL
     database: 'proyecto_graduacion',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-module.exports = pool.promise(); // Exportamos el pool como promesa para usar async/await
+module.exports = pool.promise(); // Se exporta el pool como promesa para usar async/await
